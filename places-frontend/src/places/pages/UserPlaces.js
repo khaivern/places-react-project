9f1203e1031c3e9caa97ee00690403b048c66159
khaivern <57806253+khaivern@users.noreply.github.com>
2021-12-25 02:49:30 +0800

    get places by user id is dynamic now

--- places-frontend/src/places/pages/UserPlaces.js
+++ places-frontend/src/places/pages/UserPlaces.js
@@ -1,44 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import ErrorModal from '../../shared/components/UIElements/ErrorModal';
+import LoadingSpinner from '../../shared/components/UIElements/Spinner/LoadingSpinner';
+import { useHttpClient } from '../../shared/hooks/http-hook';
 
 import PlaceList from '../components/PlaceList';
 
-const DUMMY_PLACES = [
-  {
-    id: 'p1',
-    title: "YOUR MOM'S HOUSE",
-    address: 'Free Trade Zone Batu Berendam, Batu Berendam, 75350 Malacca',
-    description: 'One of the most visited places by a lot of giga chads',
-    imageUrl:
-      'https://i.picsum.photos/id/308/200/300.jpg?hmac=gixbOWHWb-aG6q4H8cIfry9U7rYooifzOLag_k5v-tk',
-    location: {
-      lat: 40.748,
-      lng: -73.987,
-    },
-    creator: 'u1',
-  },
-  {
-    id: 'p2',
-    title: 'Perry the platypus',
-    address: 'Taman Perindustrian Batu Berendam, 75350 Malacca',
-    description: 'the detective perry, blue and soy',
-    imageUrl:
-      'https://i.picsum.photos/id/404/200/300.jpg?hmac=1i6ra6DJN9kJ9AQVfSf3VD1w08FkegBgXuz9lNDk1OM',
-    location: {
-      lat: 2.2401141,
-      lng: 102.2891321,
-    },
-    creator: 'u2',
-  },
-];
-
 const UserPlaces = () => {
-  const params = useParams();
+  const { isLoading, error, sendRequest, clearError } = useHttpClient();
+  const [places, setPlaces] = useState();
+  const userId = useParams().userId;
 
-  const loadedPlaces = DUMMY_PLACES.filter(
-    place => place.creator === params.userId
+  useEffect(() => {
+    const fetchPlaces = async () => {
+      try {
+        const data = await sendRequest(
+          'http://localhost:5000/api/places/user/' + userId
+        );
+        setPlaces(data.places);
+      } catch (err) {}
+    };
+    fetchPlaces();
+  }, [sendRequest, userId]);
+
+  return (
+    <>
+      <ErrorModal error={error} onClear={clearError} />
+      {isLoading && <LoadingSpinner asOverlay />}
+      {!isLoading && places && <PlaceList items={places} />}
+    </>
   );
-  return <PlaceList items={loadedPlaces} />;
 };
 
 export default UserPlaces;
