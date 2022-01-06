60a22091ad59cff58bfa6cd7dbd41f19927ffca2
khaivern <57806253+khaivern@users.noreply.github.com>
2022-01-06 21:18:25 +0800

    input component + new place page created

--- frontend/src/places/pages/NewPlace.tsx
+++ frontend/src/places/pages/NewPlace.tsx
@@ -1,8 +1,130 @@
+import { useReducer, useCallback } from 'react';
+import Button from '../../shared/components/FormElements/Button';
+import Input from '../../shared/components/FormElements/Input';
+
+import { VALIDATOR_MINLENGTH, VALIDATOR_REQUIRE } from '../../util/validators';
+import './NewPlace.css';
+
+const initialInputString = {
+  value: '',
+  isValid: false,
+};
+
+interface initialFormI {
+  inputs: { [id: string]: { value: string; isValid: boolean } };
+  overallIsValid: boolean;
+}
+
+const initialForm: initialFormI = {
+  inputs: {
+    title: initialInputString,
+    description: initialInputString,
+    address: initialInputString,
+    image: initialInputString,
+  },
+  overallIsValid: false,
+};
+
+interface FormActionChange {
+  type: 'FORM_INPUT_HANDLER';
+  id: string;
+  val: string;
+  isValid: boolean;
+}
+
+type Action = FormActionChange;
+
+const formReducer = (state = initialForm, action: Action) => {
+  switch (action.type) {
+    case 'FORM_INPUT_HANDLER':
+      let overallFormValidity = true;
+      for (const inputId in state.inputs) {
+        if (inputId === action.id) {
+          overallFormValidity = overallFormValidity && action.isValid;
+        } else {
+          const currentInputId = state.inputs[inputId];
+          overallFormValidity = overallFormValidity && currentInputId.isValid;
+        }
+      }
+      return {
+        ...state,
+        inputs: {
+          ...state.inputs,
+          [action.id]: {
+            value: action.val,
+            isValid: action.isValid,
+          },
+        },
+        overallIsValid: overallFormValidity,
+      };
+    default:
+      return state;
+  }
+};
+
 const NewPlace = () => {
+  const [{ inputs, overallIsValid }, dispatchFormAction] = useReducer(
+    formReducer,
+    initialForm
+  );
+
+  const inputHandler = useCallback(
+    (id: string, value: string, isValid: boolean) => {
+      dispatchFormAction({
+        type: 'FORM_INPUT_HANDLER',
+        id: id,
+        val: value,
+        isValid: isValid,
+      });
+    },
+    []
+  );
+
+  const submitNewPlaceHandler: React.FormEventHandler<HTMLFormElement> = (
+    e
+  ) => {
+    e.preventDefault();
+    console.log(inputs);
+  };
+
   return (
-    <div>
-      <h1>New Place</h1>
-    </div>
+    <form className='place-form' onSubmit={submitNewPlaceHandler}>
+      <Input
+        element='input'
+        type='text'
+        id='title'
+        label='Title'
+        validators={[VALIDATOR_REQUIRE()]}
+        errorText='Please enter a valid title!'
+        onInput={inputHandler}
+      />
+      <Input
+        element='textarea'
+        id='description'
+        label='Description'
+        validators={[VALIDATOR_MINLENGTH(4)]}
+        errorText='Please enter a valid description!'
+        onInput={inputHandler}
+      />
+      <Input
+        element='input'
+        type='text'
+        id='image'
+        label='Image'
+        validators={[VALIDATOR_REQUIRE()]}
+        errorText='Please enter a valid image!'
+        onInput={inputHandler}
+      />
+      <Input
+        element='textarea'
+        id='address'
+        label='Address'
+        validators={[VALIDATOR_REQUIRE()]}
+        errorText='Please enter a valid address!'
+        onInput={inputHandler}
+      />
+      <Button disabled={!overallIsValid}>Submit Form Data</Button>
+    </form>
   );
 };
 
---
+++ frontend/src/shared/components/FormElements/Input.tsx
@@ -0,0 +1,111 @@
+import React, { useEffect, useReducer } from 'react';
+
+import { validate } from '../../../util/validators';
+import './Input.css';
+
+interface InputProps {
+  element: string;
+  id: string;
+  rows?: number;
+  type?: string;
+  label: string;
+  validators: { type: string; val?: number }[];
+  errorText: string;
+  onInput: (id: string, value: string, isValid: boolean) => void;
+}
+
+const initialInput = {
+  value: '',
+  isValid: false,
+  isTouched: false,
+};
+
+interface InputActionChange {
+  type: 'CHANGE';
+  val: string;
+  validators: { type: string; val?: number }[];
+}
+
+interface InputActionBlur {
+  type: 'BLUR';
+}
+
+type InputAction = InputActionChange | InputActionBlur;
+
+const inputReducer = (state = initialInput, action: InputAction) => {
+  switch (action.type) {
+    case 'CHANGE':
+      return {
+        ...state,
+        value: action.val,
+        isValid: validate(action.val, action.validators),
+      };
+    case 'BLUR':
+      return { ...state, isTouched: true };
+    default:
+      return state;
+  }
+};
+
+const Input: React.FC<InputProps> = ({
+  element,
+  type,
+  id,
+  rows,
+  label,
+  validators,
+  errorText,
+  onInput,
+}) => {
+  const [{ value, isValid, isTouched }, dispatchInputAction] = useReducer(
+    inputReducer,
+    initialInput
+  );
+
+  useEffect(() => {
+    onInput(id, value, isValid);
+  }, [onInput, id, value, isValid]);
+
+  const inputChangeHandler: React.ChangeEventHandler<
+    HTMLInputElement | HTMLTextAreaElement
+  > = (e) => {
+    dispatchInputAction({
+      type: 'CHANGE',
+      val: e.target.value,
+      validators: validators,
+    });
+  };
+
+  const inputBlurHandler = () => {
+    dispatchInputAction({ type: 'BLUR' });
+  };
+
+  const content =
+    element === 'input' ? (
+      <input
+        type={type || 'text'}
+        id={id}
+        onChange={inputChangeHandler}
+        onBlur={inputBlurHandler}
+      />
+    ) : (
+      <textarea
+        id={id}
+        rows={rows || 3}
+        onChange={inputChangeHandler}
+        onBlur={inputBlurHandler}
+      />
+    );
+
+  const hasError = isTouched && !isValid;
+
+  return (
+    <div className={`form-control ${hasError ? 'form-control--invalid' : ''}`}>
+      <label htmlFor={id}>{label}</label>
+      {content}
+      {hasError && <p>{errorText}</p>}
+    </div>
+  );
+};
+
+export default Input;
